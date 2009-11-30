9898b38a2609468a57bb89435748486c8611b129
titilima <titilima@89554c5a-91f9-11de-87e2-8d177609176d>
2009-11-30 05:53:51 +0000

    [179] 加入默认的消息处理宏.

--- include/pdl_message.h
+++ include/pdl_message.h
@@ -56,32 +56,41 @@
     else if (WM_CLOSE == uMsg) {        \
         fn(bHandled);                   \
     }
+#define DEFAULT_CLOSE_HANDLER()         \
+    DoDefault(WM_CLOSE, 0, 0)
 
 // WM_COMMAND
 
-#define DECLARE_COMMAND_HANDLER(fn)     \
-    void fn(WORD wNotifyCode, WORD wID, \
+#define DECLARE_COMMAND_HANDLER(fn)         \
+    void fn(WORD wNotifyCode, WORD wID,     \
         HWND hWndCtrl, BOOL& bHandled);
-#define PROCESS_COMMAND(fn)             \
-    else if (WM_COMMAND == uMsg) {      \
-        fn(HIWORD(wParam),              \
-            LOWORD(wParam),             \
-            (HWND)lParam,               \
-            bHandled);                  \
+#define PROCESS_COMMAND(fn)                 \
+    else if (WM_COMMAND == uMsg) {          \
+        fn(HIWORD(wParam),                  \
+            LOWORD(wParam),                 \
+            (HWND)lParam,                   \
+            bHandled);                      \
     }
+#define DEFAULT_COMMAND_HANDLER(n, id, h)   \
+    DoDefault(WM_COMMAND,                   \
+        MAKEWPARAM(id, n),                  \
+        (LPARAM)h)
 
 // WM_CONTEXTMENU
 
-#define DECLARE_CONTEXTMENU_HANDLER(fn) \
-    void fn(HWND hWnd, int x, int y,    \
+#define DECLARE_CONTEXTMENU_HANDLER(fn)         \
+    void fn(HWND hWnd, int x, int y,            \
         BOOL& bHandled);
-#define PROCESS_CONTEXTMENU(fn)         \
-    else if (WM_CONTEXTMENU == uMsg) {  \
-        fn((HWND)wParam,                \
-            LOWORD(lParam),             \
-            HIWORD(lParam),             \
-            bHandled);                  \
+#define PROCESS_CONTEXTMENU(fn)                 \
+    else if (WM_CONTEXTMENU == uMsg) {          \
+        fn((HWND)wParam,                        \
+            LOWORD(lParam),                     \
+            HIWORD(lParam),                     \
+            bHandled);                          \
     }
+#define DEFAULT_CONTEXTMENU_HANDLER(h, x, y)    \
+    DoDefault(WM_CONTEXTMENU, (WPARAM)h,        \
+        MAKELPARAM(x, y))
 
 // WM_CREATE
 
@@ -94,6 +103,8 @@
             (LPCREATESTRUCT)lParam,     \
             bHandled);                  \
     }
+#define DEFAULT_CREATE_HANDLER(cs)      \
+    DoDefault(WM_CREATE, 0, (LPARAM)cs)
 
 // WM_DESTROY
 
@@ -103,6 +114,8 @@
     else if (WM_DESTROY == uMsg) {      \
         fn(bHandled);                   \
     }
+#define DEFAULT_DESTROY_HANDLER()       \
+    DoDefault(WM_DESTROY, 0, 0)
 
 // WM_DROPFILES
 
@@ -113,6 +126,9 @@
     else if (WM_DROPFILES == uMsg) {    \
         fn((HDROP)wParam, bHandled);    \
     }
+#define DEFAULT_DROPFILES_HANDLER(hd)   \
+    DoDefault(WM_DROPFILES,             \
+        (WPARAM)hd), 0)
 
 // WM_ERASEBKGND
 
@@ -123,31 +139,39 @@
         ret = (LRESULT)fn((HDC)wParam,  \
             bHandled);                  \
     }
+#define DEFAULT_ERASEBKGND_HANDLER(h)   \
+    DoDefault(WM_ERASEBKGND,            \
+        (WPARAM)h, 0)
 
 // WM_INITDIALOG
 
-#define DECLARE_INITDIALOG_HANDLER(fn)  \
-    BOOL fn(HWND hCtrlFocus,            \
+#define DECLARE_INITDIALOG_HANDLER(fn)      \
+    BOOL fn(HWND hCtrlFocus,                \
         LPARAM lParam, BOOL& bHandled);
-#define PROCESS_INITDIALOG(fn)          \
-    else if (WM_INITDIALOG == uMsg) {   \
-        ret = (LRESULT)fn((HWND)wParam, \
-            lParam, bHandled);          \
+#define PROCESS_INITDIALOG(fn)              \
+    else if (WM_INITDIALOG == uMsg) {       \
+        ret = (LRESULT)fn((HWND)wParam,     \
+            lParam, bHandled);              \
     }
+#define DEFAULT_INITDIALOG_HANDLER(h, lp)   \
+    DoDefault(WM_INITDIALOG, (WPARAM)h, lp)
 
 // WM_KEYDOWN
 
-#define DECLARE_KEYDOWN_HANDLER(fn)     \
-    void fn(UINT nChar,                 \
-        UINT nRepCnt, UINT nFlags,      \
+#define DECLARE_KEYDOWN_HANDLER(fn)         \
+    void fn(UINT nChar,                     \
+        UINT nRepCnt, UINT nFlags,          \
         BOOL& bHandled);
-#define PROCESS_KEYDOWN(fn)             \
-    else if (WM_KEYDOWN == uMsg) {      \
-        fn((UINT)wParam,                \
-            LOWORD(lParam),             \
-            HIWORD(lParam),             \
-            bHandled);                  \
+#define PROCESS_KEYDOWN(fn)                 \
+    else if (WM_KEYDOWN == uMsg) {          \
+        fn((UINT)wParam,                    \
+            LOWORD(lParam),                 \
+            HIWORD(lParam),                 \
+            bHandled);                      \
     }
+#define DEFAULT_KEYDOWN_HANDLER(c, r, f)    \
+    DoDefault(WM_KEYDOWN, (WPARAM)c,        \
+        MAKELPARAM(r, f))
 
 // WM_KEYUP
 
@@ -162,45 +186,57 @@
             HIWORD(lParam),             \
             bHandled);                  \
     }
+#define DEFAULT_KEYUP_HANDLER(c, r, f)  \
+    DoDefault(WM_KEYUP, (WPARAM)c,      \
+        MAKELPARAM(r, f))
 
 // WM_LBUTTONDBLCLK
 
-#define DECLARE_LBUTTONDBLCLK_HANDLER(fn)   \
+#define DECLARE_LBUTTONDBLCLK_HANDLER(fn)       \
+    void fn(UINT uFlags, int x, int y,          \
+        BOOL& bHandled);
+#define PROCESS_LBUTTONDBLCLK(fn)               \
+    else if (WM_LBUTTONDBLCLK == uMsg) {        \
+        fn((UINT)wParam,                        \
+            GET_X_LPARAM(lParam),               \
+            GET_Y_LPARAM(lParam),               \
+            bHandled);                          \
+    }
+#define DEFAULT_LBUTTONDBLCLK_HANDLER(f, x, y)  \
+    DoDefault(WM_LBUTTONDBLCLK, f,              \
+        MAKELPARAM(x, y))
+
+// WM_LBUTTONDOWN
+
+#define DECLARE_LBUTTONDOWN_HANDLER(fn)         \
+    void fn(UINT uFlags, int x, int y,          \
+        BOOL& bHandled);
+#define PROCESS_LBUTTONDOWN(fn)                 \
+    else if (WM_LBUTTONDOWN == uMsg) {          \
+        fn((UINT)wParam,                        \
+            GET_X_LPARAM(lParam),               \
+            GET_Y_LPARAM(lParam),               \
+            bHandled);                          \
+    }
+#define DEFAULT_LBUTTONDOWN_HANDLER(f, x, y)    \
+    DoDefault(WM_LBUTTONDOWN, f,                \
+        MAKELPARAM(x, y))
+
+// WM_LBUTTONUP
+
+#define DECLARE_LBUTTONUP_HANDLER(fn)       \
     void fn(UINT uFlags, int x, int y,      \
         BOOL& bHandled);
-#define PROCESS_LBUTTONDBLCLK(fn)           \
-    else if (WM_LBUTTONDBLCLK == uMsg) {    \
+#define PROCESS_LBUTTONUP(fn)               \
+    else if (WM_LBUTTONUP == uMsg) {        \
         fn((UINT)wParam,                    \
             GET_X_LPARAM(lParam),           \
             GET_Y_LPARAM(lParam),           \
             bHandled);                      \
     }
-
-// WM_LBUTTONDOWN
-
-#define DECLARE_LBUTTONDOWN_HANDLER(fn) \
-    void fn(UINT uFlags, int x, int y,  \
-        BOOL& bHandled);
-#define PROCESS_LBUTTONDOWN(fn)         \
-    else if (WM_LBUTTONDOWN == uMsg) {  \
-        fn((UINT)wParam,                \
-            GET_X_LPARAM(lParam),       \
-            GET_Y_LPARAM(lParam),       \
-            bHandled);                  \
-    }
-
-// WM_LBUTTONUP
-
-#define DECLARE_LBUTTONUP_HANDLER(fn)   \
-    void fn(UINT uFlags, int x, int y,  \
-        BOOL& bHandled);
-#define PROCESS_LBUTTONUP(fn)           \
-    else if (WM_LBUTTONUP == uMsg) {    \
-        fn((UINT)wParam,                \
-            GET_X_LPARAM(lParam),       \
-            GET_Y_LPARAM(lParam),       \
-            bHandled);                  \
-    }
+#define DEFAULT_LBUTTONUP_HANDLER(f, x, y)  \
+    DoDefault(WM_LBUTTONUP, f,              \
+        MAKELPARAM(x, y))
 
 // WM_MOUSELEAVE
 
@@ -210,19 +246,24 @@
     else if (WM_MOUSELEAVE == uMsg) {   \
         fn(bHandled);                   \
     }
+#define DEFAULT_MOUSELEAVE_HANDLER()    \
+    DoDefault(WM_MOUSELEAVE, 0, 0)
 
 // WM_MOUSEMOVE
 
-#define DECLARE_MOUSEMOVE_HANDLER(fn)   \
-    void fn(UINT uFlags, int x, int y,  \
+#define DECLARE_MOUSEMOVE_HANDLER(fn)       \
+    void fn(UINT uFlags, int x, int y,      \
         BOOL& bHandled);
-#define PROCESS_MOUSEMOVE(fn)           \
-    else if (WM_MOUSEMOVE == uMsg) {    \
-        fn((UINT)wParam,                \
-            GET_X_LPARAM(lParam),       \
-            GET_Y_LPARAM(lParam),       \
-            bHandled);                  \
+#define PROCESS_MOUSEMOVE(fn)               \
+    else if (WM_MOUSEMOVE == uMsg) {        \
+        fn((UINT)wParam,                    \
+            GET_X_LPARAM(lParam),           \
+            GET_Y_LPARAM(lParam),           \
+            bHandled);                      \
     }
+#define DEFAULT_MOUSEMOVE_HANDLER(f, x, y)  \
+    DoDefault(WM_MOUSEMOVE, f,              \
+        MAKELPARAM(x, y))
 
 // WM_NOTIFY
 
@@ -234,6 +275,9 @@
         ret = fn(wParam,                \
             (LPNMHDR)lParam, bHandled); \
     }
+#define DEFAULT_NOTIFY_HANDLER(id, hdr) \
+    DoDefault(WM_NOTIFY, id,            \
+        (LPARAM)hdr)
 
 // WM_PAINT
 
@@ -243,32 +287,56 @@
     else if (WM_PAINT == uMsg) {        \
         fn(bHandled);                   \
     }
+#define DEFAULT_PAINT_HANDLER()         \
+    DoDefault(WM_PAINT, 0, 0)
 
 // WM_RBUTTONDOWN
 
-#define DECLARE_RBUTTONDOWN_HANDLER(fn) \
-    void fn(UINT uFlags, int x, int y,  \
+#define DECLARE_RBUTTONDOWN_HANDLER(fn)         \
+    void fn(UINT uFlags, int x, int y,          \
         BOOL& bHandled);
-#define PROCESS_RBUTTONDOWN(fn)         \
-    else if (WM_RBUTTONDOWN == uMsg) {  \
-        fn((UINT)wParam,                \
-            GET_X_LPARAM(lParam),       \
-            GET_Y_LPARAM(lParam),       \
-            bHandled);                  \
+#define PROCESS_RBUTTONDOWN(fn)                 \
+    else if (WM_RBUTTONDOWN == uMsg) {          \
+        fn((UINT)wParam,                        \
+            GET_X_LPARAM(lParam),               \
+            GET_Y_LPARAM(lParam),               \
+            bHandled);                          \
     }
+#define DEFAULT_RBUTTONDOWN_HANDLER(f, x, y)    \
+    DoDefault(WM_RBUTTONDOWN, f,                \
+        MAKELPARAM(x, y))
+
+// WM_RBUTTONUP
+
+#define DECLARE_RBUTTONUP_HANDLER(fn)       \
+    void fn(UINT uFlags, int x, int y,      \
+        BOOL& bHandled);
+#define PROCESS_RBUTTONUP(fn)               \
+    else if (WM_RBUTTONUP == uMsg) {        \
+        fn((UINT)wParam,                    \
+            GET_X_LPARAM(lParam),           \
+            GET_Y_LPARAM(lParam),           \
+            bHandled);                      \
+    }
+#define DEFAULT_RBUTTONUP_HANDLER(f, x, y)  \
+    DoDefault(WM_RBUTTONUP, f,              \
+        MAKELPARAM(x, y))
 
 // WM_SETCURSOR
 
-#define DECLARE_SETCURSOR_HANDLER(fn)   \
-    BOOL fn(HWND hWnd, UINT nHitTest,   \
+#define DECLARE_SETCURSOR_HANDLER(fn)                   \
+    BOOL fn(HWND hWnd, UINT nHitTest,                   \
         UINT message, BOOL& bHandled);
-#define PROCESS_SETCURSOR(fn)           \
-    else if (WM_SETCURSOR == uMsg) {    \
-        ret = fn((HWND)wParam,          \
-            LOWORD(lParam),             \
-            HIWORD(lParam),             \
-            bHandled);                  \
+#define PROCESS_SETCURSOR(fn)                           \
+    else if (WM_SETCURSOR == uMsg) {                    \
+        ret = fn((HWND)wParam,                          \
+            LOWORD(lParam),                             \
+            HIWORD(lParam),                             \
+            bHandled);                                  \
     }
+#define DEFAULT_SETCURSOR_HANDLER(hwnd, hittest, m)     \
+    DoDefault(WM_SETCURSOR, (WPARAM)hwnd,               \
+        MAKELPARAM(hittest, m))
 
 // WM_SIZE
 
@@ -282,3 +350,19 @@
             HIWORD(lParam),             \
             bHandled);                  \
     }
+#define DEFAULT_SIZE_HANDLER(n, cx, cy) \
+    DoDefault(WM_SIZE, n,               \
+        MAKELPARAM(cx, cy))
+
+// WM_TIMER
+
+#define DECLARE_TIMER_HANDLER(fn)       \
+    void fn(UINT_PTR nIDEvent,          \
+        BOOL& bHandled);
+#define PROCESS_TIMER(fn)               \
+    else if (WM_TIMER == uMsg) {        \
+        fn((UINT)wParam,  bHandled);    \
+        ret = 0;                        \
+    }
+#define DEFAULT_TIMER_HANDLER(id)       \
+    DoDefault(WM_TIMER, id, 0)
