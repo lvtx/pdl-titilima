cf92c5a9847099dac2817111bb477e125f31045e
titilima <titilima@89554c5a-91f9-11de-87e2-8d177609176d>
2009-12-30 10:02:01 +0000

    增加 LPopupMenu 类.

--- trunk/include/pdl_menu.h
+++ trunk/include/pdl_menu.h
@@ -1,6 +1,9 @@
 /**
  * \file pdl_menu.h
- * \brief PDL 菜单封装类 
+ * \brief PDL 菜单封装
+ * \details 这个文件中包含了对菜单的各种封装：
+ *   \li \c LMenu PDL 菜单封装类
+ *   \li \c LPopupMenu PDL 弹出菜单封装类
  */
 
 #pragma once
@@ -21,30 +24,48 @@ class LMenu
 {
 public:
     LMenu(__in HMENU hMenu = NULL);
-    operator HMENU(void);
+    operator HMENU(void) const;
     LMenu& operator=(__in HMENU hMenu);
 public:
     DWORD CheckItem(__in UINT uIDCheckItem, __in UINT uCheck);
 #ifdef _WIN32_WCE
     static HWND PDLAPI CreateBarCE(__in HWND hParent, __in UINT uId);
 #endif // _WIN32_WCE
+    BOOL Destroy(void);
     BOOL EnableItem(__in UINT uIDEnableItem, __in UINT uEnable);
     BOOL GetItemInfo(__in UINT uItem, __in BOOL fByPosition,
         __inout LPMENUITEMINFOA lpmii);
     BOOL GetItemInfo(__in UINT uItem, __in BOOL fByPosition,
         __inout LPMENUITEMINFOW lpmii);
     HMENU GetSub(__in int nPos);
+    BOOL IsMenu(void);
     BOOL LoadLanguageRes(__in LIniParser* lan, __in PCSTR name);
+    BOOL Remove(__in UINT uPosition, __in UINT uFlags);
     BOOL SetDefaultItem(__in UINT uItem, __in UINT fByPos);
     BOOL SetItemInfo(__in UINT uItem, __in BOOL fByPosition,
         __in LPMENUITEMINFOA lpmii);
     BOOL SetItemInfo(__in UINT uItem, __in BOOL fByPosition,
         __in LPMENUITEMINFOW lpmii);
-    BOOL TrackPopup(__in UINT uFlags, __in int x, __in int y,
-        __in HWND hWnd);
 protected:
     static void LoadLanguageRes(LIniParser* lan, PCSTR name, HMENU hSubMenu,
         int& idxPopup);
 protected:
     HMENU m_hMenu;
+};
+
+/**
+ * \class LPopupMenu
+ * \brief PDL 弹出菜单封装类
+ */
+
+class LPopupMenu : public LMenu
+{
+public:
+    LPopupMenu(__in HMENU hMenu = NULL);
+    ~LPopupMenu(void);
+    operator HMENU(void) const;
+    LPopupMenu& operator=(__in HMENU hMenu);
+public:
+    BOOL Create(void);
+    BOOL Track(__in UINT uFlags, __in int x, __in int y, __in HWND hWnd);
 };
